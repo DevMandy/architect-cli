72769170c9e3e76c819413bb5b9e275732546dba
Mandy Hubbard <devmandy@yahoo.com>
2022-03-13 18:25:50 -0500

    Added secrets command

---
+++ src/commands/secrets/index.ts
@@ -0,0 +1,30 @@
+import Account from '../../architect/account/account.entity';
+import AccountUtils from '../../architect/account/account.utils';
+import Command from '../../base-command';
+import {EnvironmentUtils} from "../../architect/environment/environment.utils";
+import yaml from 'js-yaml';
+
+export default class SecretsGet extends Command {
+  async auth_required(): Promise<boolean> {
+    return true;
+  }
+
+  static aliases = ['secrets:get'];
+  static description = 'Get secrets for a specified account and environment';
+
+  static flags = {
+    ...AccountUtils.flags,
+    ...EnvironmentUtils.flags,
+  };
+
+  async run(): Promise<void> {
+    const { flags } = await this.parse(SecretsGet);
+
+    const account: Account = await AccountUtils.getAccount(this.app, flags.account);
+    const environment = await EnvironmentUtils.getEnvironment(this.app.api, account, flags.environment);
+
+    const { data: secrets } = await this.app.api.get(`/environments/${environment.id}/secrets/values`)
+    this.log(yaml.dump(secrets));
+  }
+}
+
